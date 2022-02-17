ff378c76932794de0fcf3f8abb1a826600d8692e
ArvikVan <arvikvan@gmail.com>
2022-02-17 09:49:34 +0300

    initNewRepo

---
+++ src/main/java/rest/config/MyConfig.java
@@ -0,0 +1,61 @@
+package rest.config;
+
+import com.mchange.v2.c3p0.ComboPooledDataSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.orm.hibernate5.HibernateTransactionManager;
+import org.springframework.orm.hibernate5.LocalSessionFactoryBean;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+import javax.sql.ConnectionPoolDataSource;
+import javax.sql.DataSource;
+import java.beans.PropertyVetoException;
+import java.util.Properties;
+
+/**
+ * @author ArvikV
+ * @version 1.0
+ * @since 17.02.2022
+ */
+@Configuration
+@ComponentScan(basePackages = "rest")
+@EnableWebMvc
+@EnableTransactionManagement
+public class MyConfig {
+    @Bean
+    public DataSource dataSource() {
+        ComboPooledDataSource dataSource = new ComboPooledDataSource();
+        try {
+            dataSource.setDriverClass("org.postgresql.Driver");
+            dataSource.setJdbcUrl("jdbc:postgresql://127.0.0.1:5432/restex");
+            dataSource.setUser("postgres");
+            dataSource.setPassword("Arvijka");
+        } catch (PropertyVetoException e) {
+            e.printStackTrace();
+        }
+        return dataSource;
+    }
+
+    @Bean
+    public LocalSessionFactoryBean sessionFactory() {
+        LocalSessionFactoryBean localSessionFactory = new LocalSessionFactoryBean();
+        localSessionFactory.setDataSource(dataSource());
+        sessionFactory().setPackagesToScan("rest.entity");
+        Properties hibernateProperties = new Properties();
+        hibernateProperties.setProperty("hibernate.dialect", "org.hibernate.dialect.PostgreSQL10Dialect");
+        hibernateProperties.setProperty("hibernate.show_sql", "true");
+
+        sessionFactory().setHibernateProperties(hibernateProperties);
+        return sessionFactory();
+    }
+
+    @Bean
+    public HibernateTransactionManager transactionManager() {
+        HibernateTransactionManager transactionManager = new HibernateTransactionManager();
+        transactionManager.setSessionFactory(sessionFactory().getObject());
+
+        return transactionManager;
+    }
+}
---
+++ src/main/java/rest/config/MyWebInitializer.java
@@ -0,0 +1,25 @@
+package rest.config;
+
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+/**
+ * @author ArvikV
+ * @version 1.0
+ * @since 17.02.2022
+ */
+public class MyWebInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+    @Override
+    protected Class<?>[] getRootConfigClasses() {
+        return null;
+    }
+
+    @Override
+    protected Class<?>[] getServletConfigClasses() {
+        return new Class[]{MyConfig.class};
+    }
+
+    @Override
+    protected String[] getServletMappings() {
+        return new String[]{"/"};
+    }
+}
