dae0322b791a7abd10d90b9874ed382f5df4de90
ArvikVan <arvikvan@gmail.com>
2022-03-12 11:28:01 +0300

    exception_handler_added

--- README.md
+++ README.md
@@ -1,16 +1,23 @@
 [![CI](https://github.com/ArvikVan/restex/actions/workflows/Createmain.yml/badge.svg?branch=master)](https://github.com/ArvikVan/restex/actions/workflows/Createmain.yml)
 ## restex
-
+работа с джсонами, обработкой исключений
+<br> выход за переделы имеющихся значений в бд
+![](img/1.png)
+<br> список полный из бд
+![](img/2.png)
+<br> выбор по id
+![](img/3.png)
+<br> ввод некорректного id
+![](img/4.png)
 ### Контакты для связи<br>
 <img src="https://img.icons8.com/clouds/100/000000/gmail-new.png" width="10"/> arvikvan@gmail.com<br>
 <img src="https://img.icons8.com/color/100/000000/telegram-app--v2.png" width="10"/> @ArvikV
 
 ###Используемые технологии:
 - Maven
-- Java EE Servlet API
 - Hibernate
 - PostgreSQL
 - Travis CI
 - Apache Tomcat
-- Spring MVC, Security, Boot, REST
+- Spring MVC, REST
 
--- src/main/java/rest/controller/MyRestController.java
+++ src/main/java/rest/controller/MyRestController.java
@@ -1,12 +1,13 @@
 package rest.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import rest.entity.Employee;
+import rest.exception.EmployeeIncorrectData;
+import rest.exception.NoEmployeeException;
 import rest.repository.EmployeeRepository;
 import rest.service.EmployeeService;
 
@@ -32,6 +33,34 @@ public class MyRestController {
     @GetMapping("/employees/{id}")
     public Employee getEmployee(@PathVariable int id) { /*для получения переменной из запроса*/
         Employee employee = employeeService.getEmployee(id);
+        if (employee == null) {
+            throw new NoEmployeeException("No employee with id " + id + " in DB");
+        }
         return employee;
+    }
+
+    /**
+     * назначаем ответственного за обработку исключений
+     * ResponseEntity обертка над HTTPresponse
+     * в обощении тип объекта который добавляется в HTTP response body, когдавы брасывается наш ексепшн
+     * EmployeeIncorrectData NoEmployeeException создать для обработки исключения
+     */
+    @ExceptionHandler
+    public ResponseEntity<EmployeeIncorrectData> handleExcepltion(NoEmployeeException exception) {
+        EmployeeIncorrectData data = new EmployeeIncorrectData();
+        data.setInfo(exception.getMessage());
+        return new ResponseEntity<>(data, HttpStatus.NOT_FOUND);
+    }
+
+    /**
+     * обрабатываем остальные ексепшены
+     * @param exception ексепшн
+     * @return на выходе ответ ексепшена
+     */
+    @ExceptionHandler
+    public ResponseEntity<EmployeeIncorrectData> handleExcepltion(Exception exception) {
+        EmployeeIncorrectData data = new EmployeeIncorrectData();
+        data.setInfo(exception.getMessage());
+        return new ResponseEntity<>(data, HttpStatus.BAD_REQUEST);
     }
 }
